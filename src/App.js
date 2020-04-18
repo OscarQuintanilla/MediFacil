a1179393115ffae6b6896ff1a0605130aad26762
Edward Servellón <edexorsoft@gmail.com>
2020-04-17 23:37:52 -0600

    Plantilla
    Solucionando el error de la plantilla

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Main_content from './components/Main_Content';
+import Main_Content from './components/Main_Content';
 
 class App extends Component {
 
   render() {
     return (
       <div>
-        Hola
+        <Header />
+        <Main_Content />
+        <Footer />
+        
       </div>
     );
   }
 }
+
 export default App;
